2c00ecffce028f2e88e50e608d89be6d46566393
tarek.stauffer <tarek.stauffer@csbe.ch>
2019-03-27 20:57:24 +0100

    pokerface from lady gaga

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,46 @@
 "use strict";
 exports.__esModule = true;
 var express = require("express");
+var gm = require("gm");
+var multer = require("multer");
 var app = express();
-app.get('/', function (req, res) {
+var storage1 = multer.diskStorage({
+    destination: function (req, res, cb) {
+        cb(null, __dirname);
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.originalname);
+    }
+});
+var upload = multer({ storage: storage1 });
+app.get('/home', function (req, res) {
     res.sendFile(__dirname + '/index.html');
 });
 app.listen(process.env.PORT || 4000, function () {
     console.log('Your node js server is running');
+});
+app.post('/api/files', upload.single('files'), function (req, res, next) {
+    console.log(req);
+    gm(req.file.filename)
+        .resize(300, null)
+        .write('./changed/medium/' + req.file.filename, function (err) {
+        if (err)
+            console.log(err);
+    });
+    gm(req.file.filename)
+        .resize(200, null)
+        .write('./changed/small/' + req.file.filename, function (err) {
+        if (err)
+            console.log(err);
+    });
+    gm(req.file.filename)
+        .resize(400, null)
+        .write('./changed/large/' + req.file.filename, function (err) {
+        if (err)
+            console.log(err);
+    });
+    //Get File.original
+    //Do stuff with gm
+    //Filetypes
+    res.sendStatus(200);
 });
--- server.ts
+++ server.ts
@@ -17,32 +17,32 @@ const storage1 = multer.diskStorage({
 const upload = multer({ storage: storage1 });
 
 app.get('/home', function(req: express.Request, res: express.Response) {
-    res.sendFile(__dirname + '/index.html');
+    res.sendFile(__dirname + './index.html');
   });
   
   app.listen(process.env.PORT || 4000, function(){
     console.log('Your node js server is running');
   });
   app.post('/api/files', upload.single('files'),function(req: express.Request, res,next){
-    console.log(req);
-    gm(req.file.filename)
-    .resize(300, null)
-    .write('./changed/medium/' + req.file.filename, function (err) {
-      if (err)  console.log(err);
-     
-    })
-    gm(req.file.filename)
-    .resize(200, null)
-    .write('./changed/small/' + req.file.filename, function (err) {
-      if (err)   console.log(err);
-     
-    })
-    gm(req.file.filename)
-    .resize(400, null)
-    .write('./changed/large/'+req.file.filename, function (err) {
-      if (err)   console.log(err);
     
+    gm('./Images/AestheticBottle.jpg')
+    .resize(300, null)
+    .write(__dirname + '/changed/medium/' + req.file.filename, function (err) {
+      if (err) console.dir(' hooray! ')
+      console.log(err);
     })
+    gm('./Images/AestheticBottle.jpg')
+    .resize(200, null)
+    .write('./changed/small/', req.file.filename), function (err) {
+      if (err) console.dir('yuh')
+      console.log(err);
+    }
+    gm('./Images/AestheticBottle.jpg')
+    .resize(400, null)
+    .write('./changed/large/', req.file.filename), function (err) {
+      if (err) console.dir('yuhh')
+      console.log(err);
+    }
     //Get File.original
     //Do stuff with gm
     //Filetypes
