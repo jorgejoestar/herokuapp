adc716b8c62f180367de9df5f6854251d94b6d75
tarek.stauffer <tarek.stauffer@csbe.ch>
2019-03-27 11:32:49 +0100

    allstar from smash mouf

--- gulpfile.ts
+++ gulpfile.ts
@@ -4,6 +4,18 @@ import * as multer from "multer";
 
 const app = express();
 
+
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, '/tmp/my-uploads')
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.fieldname + '-' + Date.now())
+  }
+})
+ 
+var upload = multer({ storage: storage })
+
 app.get('/home', function(req: express.Request, res: express.Response) {
     res.sendFile(__dirname + '/index.html');
   });
@@ -11,3 +23,9 @@ app.get('/home', function(req: express.Request, res: express.Response) {
   app.listen(process.env.PORT || 4000, function(){
     console.log('Your node js server is running');
   });
+  app.post('/api/files', upload.single('files'),function(req,res){
+    
+    //Get File.original
+    //Do stuff with gm
+    //Filetypes
+  })
